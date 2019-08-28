f51610e813a8dbfec8eb4d3828279756b4748c4c
nerrewton <nerrechiboy@gmail.com>
2019-08-28 00:22:06 -0500

    Se quita metodo no permitido del api

--- api/controllers/RespuestaHttpController.class.php
+++ api/controllers/RespuestaHttpController.class.php
@@ -4,7 +4,7 @@ class RespuestaHttpController
     public function devolver( $estado = 200, $datos = "" ){
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         header("Allow: GET, POST, PUT, DELETE");
         header("Content-Type: application/json");
         print_r( json_encode($datos) );
